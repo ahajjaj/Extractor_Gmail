ba0b2bf25a24218ef4c2e67156d631f9b83a22e1
HAJJAJ <45050815+ahajjaj@users.noreply.github.com>
2020-07-21 11:26:00 +0200

    Update extractor.config.php

--- extractor.config.php
+++ extractor.config.php
@@ -4,7 +4,7 @@ require_once '.env';
 
 $inbox = imap_open($ENV['HOSTNAME'],$ENV['USERNAME'],$ENV['PASSWORD']) or die('Cannot connect to Gmail: ' . imap_last_error());
 
-$emails = imap_search($inbox,'FROM "ahmedhajjaj25@gmail.com"');
+$emails = imap_search($inbox,'FROM "@gmail.com"');
 $emails_s = imap_search($inbox,'SUBJECT "JDMA"');
 $pathToSave = "local_server/client1/";
 $trashBox = '[Gmail]/Corbeille';
